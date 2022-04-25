f1872ed317ffa942cdda1cfc057539b75371b0f3
Westindiess <gregoryalexiss@gmail.com>
2022-04-25 18:30:48 +0200

    deploiement final

--- server.js
+++ server.js
@@ -51,8 +51,13 @@ server.on("listening", () => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client/build"));
+    res.sendFile(path.join(__dirname, "/client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running");
   });
 }
 
