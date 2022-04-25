f8c6ad826865be39dd3978f98a8e2f6e84e939d7
Westindiess <gregoryalexiss@gmail.com>
2022-04-25 17:50:51 +0200

    re upload keep note project

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const app = require("./app");
 const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: "./env" });
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -48,9 +50,9 @@ server.on("listening", () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/client/build"));
   });
 }
 
