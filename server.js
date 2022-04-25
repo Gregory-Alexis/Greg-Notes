4d5c2750fe9dd9e7edbc9356b235ef9cc50e2083
Westindiess <gregoryalexiss@gmail.com>
2022-04-25 22:40:11 +0200

    edit middleware

--- server.js
+++ server.js
@@ -50,11 +50,7 @@ server.on("listening", () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"));
-  });
+  app.use(express.static(path.join(__dirname, "client/build")));
 } else {
   app.get("/", (req, res) => {
     res.send("Api is running");
